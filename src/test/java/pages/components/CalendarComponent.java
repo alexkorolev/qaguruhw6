398297aad202daa87a98e12a7824ebc2b995a1a3
Aleksei Korolev <alexkorolev86@gmail.com>
2023-11-19 16:39:13 +0300

    commit hw8 (data faker homework part 2)

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,12 +1,15 @@
 package pages.components;
 
-import static com.codeborne.selenide.Selectors.byText;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class CalendarComponent {
     public void setDateOfBirth(String day, String month, String year){
         $("select[class*='month-select']").selectOption(month);
         $("select[class*='year-select']").selectOption(year);
-        $(".react-datepicker__month").$(byText(day)).click();
+        $$(".react-datepicker__day:not(.react-datepicker__day--outside-month)").findBy(text(day)).click();
     }
 }
